da93c438f0f14deeeeb0aba51ec0fda456405677
trsjmx <tawfiq1113@gmail.com>
2024-09-29 16:59:31 +0300

    Present Arwa 2

--- Arwa.swift
+++ Arwa.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct Arwa: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("Arwa")
+                .resizable()
+                .frame(width: 200.0, height: 200.0)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.orange, lineWidth:4)
+                )
+            
+            Text("Arwa Ghelan")
+                .frame (maxWidth:.infinity,alignment: .leading)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.purple)
+                .multilineTextAlignment(.leading)
+                .padding()
+            Text("I am 23 years old,a Software Engineering graduate who enjoys spending time with my fameily,traveling, and coloring. I like to work in teams and reach to a perfect and outstanding results")
+        }
+        .padding()
     }
 }
 
