aeff779f664ca4cc43a12d4f49f1b7c33e840048
Renad <uniirena@gmail.com>
2024-09-29 16:24:41 +0300

    present me renad

--- Renad.swift
+++ Renad.swift
@@ -9,8 +9,38 @@ import SwiftUI
 
 struct Renad: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        ZStack{
+            Image("meee")
+                .resizable()
+                .scaledToFill()
+                .frame(maxHeight: .infinity)
+                .ignoresSafeArea()
+
+            VStack{
+                
+                Text("Present Me!")
+                    .font(.title).bold()
+                    .foregroundColor(Color.red)
+                
+                Image("mee")
+                    .resizable()
+                    .foregroundColor(.blue)
+                
+                    .frame(width:200,height:200)
+                    .clipShape(Circle())
+                    .shadow(color: .red,radius:20)
+               /* LinearGradient(colors: [.purple, .pink, .blue],startPoint: .top,endPoint: .bottom)
+                    .blendMode(.color)*/
+                HStack{
+                    Text("Hello My name is Renad Alyousef I'am 21 years old I'am currently pursuing my bachelors degree in information technology one intersting fact about me is that coffee makes me sleepy!")
+                    
+                    .foregroundColor(Color.red)}
+                    .padding()
+                
+            }
+            .padding()
+            
+        }}
 }
 
 #Preview {
