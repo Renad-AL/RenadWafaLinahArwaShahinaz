4c2b9bdb54a78753798257767ad923109e004072
Renad <uniirena@gmail.com>
2024-09-29 16:30:07 +0300

    Present Renad

---
+++ rrr.swift
@@ -0,0 +1,18 @@
+//
+//  rrr.swift
+//  RenadWafaLinahArwaShahinaz
+//
+//  Created by Renad Alyousef on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct rrr: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    rrr()
+}
