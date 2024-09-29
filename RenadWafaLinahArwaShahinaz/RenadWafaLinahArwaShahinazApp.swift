ce60d6814e8c04203de879db54fc95f46b8051b9
Renad <uniirena@gmail.com>
2024-09-29 14:48:05 +0300

    Initial Commit

---
+++ RenadWafaLinahArwaShahinaz/RenadWafaLinahArwaShahinazApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenadWafaLinahArwaShahinazApp.swift
+//  RenadWafaLinahArwaShahinaz
+//
+//  Created by Renad Alyousef on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RenadWafaLinahArwaShahinazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RenadWafaLinahArwaShahinazTests/RenadWafaLinahArwaShahinazTests.swift
@@ -0,0 +1,16 @@
+//
+//  RenadWafaLinahArwaShahinazTests.swift
+//  RenadWafaLinahArwaShahinazTests
+//
+//  Created by Renad Alyousef on 26/03/1446 AH.
+//
+
+import Testing
+
+struct RenadWafaLinahArwaShahinazTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
